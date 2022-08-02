04c7b810591698500468f875408e37e423a289d3
vldslove <gsmgodmod@gmail.com>
2022-08-02 19:28:19 +0300

    выполнил дз №6

--- src/lesson6/MainClass.java
+++ src/lesson6/MainClass.java
@@ -5,13 +5,16 @@ public class MainClass {
         CreditCard creditcard1 = new CreditCard(1,100);
         CreditCard creditcard2 = new CreditCard(2,200);
         CreditCard creditcard3 = new CreditCard(3,300);
+
         creditcard1.accountInfo();
         creditcard2.accountInfo();
         creditcard3.accountInfo();
 
         creditcard1.replenishBalance(200);
         creditcard2.replenishBalance(400);
+
         creditcard3.withdrawAmount(200);
+
         creditcard1.accountInfo();
         creditcard2.accountInfo();
         creditcard3.accountInfo();
